5d5c03782cbdcfd0cd04e816eca9a32e7bd31084
Arun Kumar <kumar05.962013@gmail.com>
2020-08-20 12:04:35 +0530

    removing useless code

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="/">
-          Navbar
-          <span className="badge badge-pill badge-secondary ml-2">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+const Navbar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="/">
+        Navbar
+        <span className="badge badge-pill badge-secondary ml-2">
+          {totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
 export default Navbar;
